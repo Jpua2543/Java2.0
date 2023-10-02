ff932d720124b8604a886f054e906df868cef62d
John <Johnpua@outlook.com>
2023-10-02 00:09:28 -0700

    adjusted Cafe.java

--- Fundamentals/Cafe Business Logic/Cafe.java
+++ Fundamentals/Cafe Business Logic/Cafe.java
@@ -1,14 +1,10 @@
 import java.util.ArrayList;
 
-public class Cafe{
+public class Cafe {
 
     public int getStreakGoal() {
-        return getStreakGoal(10);
-    }
-
-    public int getStreakGoal(int numWeeks) {
         int sum = 0;
-        for (int i = 1; i <= numWeeks; i++) {
+        for (int i = 1; i <= 10; i++) {
             sum += i;
         }
         return sum;
@@ -16,8 +12,8 @@ public class Cafe{
 
     public double getOrderTotal(double[] prices) {
         double total = 0;
-        for (double price : prices) {
-            total += price;
+        for (int i = 0; i < prices.length; i++) {
+            total = total + prices[i];
         }
         return total;
     }
@@ -31,27 +27,26 @@ public class Cafe{
     public void addCustomer(ArrayList<String> customers) {
         System.out.println("Please enter your name:");
         String username = System.console().readLine();
-        System.out.println("Hello, " + username + "!");
-        System.out.println("There are " + customers.size() + " people in front of you");
         customers.add(username);
-        System.out.println(customers);
+        System.out.println("Added: " + username);
     }
 
     public void printPriceChart(String product, double price, int maxQuantity) {
         System.out.println(product);
         for (int i = 1; i <= maxQuantity; i++) {
-            System.out.printf("%d - $%.2f\n", i, price * i);
+            System.out.println(i + " - $" + (price * i));
         }
     }
 
     public void printPriceChartWithDiscount(String product, double price, int maxQuantity) {
         System.out.println(product);
         for (int i = 1; i <= maxQuantity; i++) {
-            System.out.printf("%d - $%.2f\n", i, (price * i) - (0.5 * (i - 1) * i / 2));
+            double discount = 0.5 * i;
+            System.out.println(i + " - $" + ((price * i) - discount));
         }
     }
 
-    public boolean displayMenu(ArrayList<String> menuItems, ArrayList<Double> prices) {
+    public boolean displayMenuWithPrices(ArrayList<String> menuItems, ArrayList<Double> prices) {
         if (menuItems.size() != prices.size()) {
             return false;
         }
@@ -59,17 +54,5 @@ public class Cafe{
             System.out.println(i + " " + menuItems.get(i) + " -- $" + prices.get(i));
         }
         return true;
-    }
-
-    public void addCustomers(ArrayList<String> customers) {
-        while (true) {
-            System.out.println("Please enter customer name or type 'q' to quit:");
-            String username = System.console().readLine();
-            if ("q".equalsIgnoreCase(username)) {
-                break;
-            }
-            customers.add(username);
-            System.out.println("Added: " + username);
-        }
     }
 }
--- Fundamentals/Cafe Business Logic/TestCafe.java
+++ Fundamentals/Cafe Business Logic/TestCafe.java
@@ -1,25 +1,41 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TestCafe {
+
     public static void main(String[] args) {
-        CafeUtil appTest = new CafeUtil();
-        System.out.println("\n----- Streak Goal Test -----");
-        System.out.printf("Purchases needed by week 10: %s \n\n", appTest.getStreakGoal());
-        double[] lineItems = {3.5, 1.5, 4.0, 4.5};
-        System.out.println("----- Order Total Test-----");
-        System.out.printf("Order total: %s \n\n", appTest.getOrderTotal(lineItems));
-        ArrayList<String> menu = new ArrayList<>();
-        menu.add("drip coffee");
-        menu.add("cappuccino");
-        menu.add("latte");
-        menu.add("mocha");
-        System.out.println("----- Display Menu Test-----");
-        appTest.displayMenu(menu);
-        System.out.println("\n----- Add Customer Test-----");
+        Cafe cafe = new Cafe();
+
+        // Test getStreakGoal()
+        System.out.println(cafe.getStreakGoal()); // Expected: 55
+        System.out.println(cafe.getStreakGoal(5)); // Ninja Bonus, Expected: 15
+
+        // Test getOrderTotal()
+        double[] order = {2.5, 3.5, 4.5, 5.5};
+        System.out.println(cafe.getOrderTotal(order)); // Expected: 16.0
+
+        // Test displayMenu()
+        ArrayList<String> menu = new ArrayList<>(Arrays.asList("drip coffee", "cappuccino", "latte", "mocha"));
+        cafe.displayMenu(menu);
+
+        // Test addCustomer()
         ArrayList<String> customers = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            appTest.addCustomer(customers);
-            System.out.println("\n");
-        }
+        cafe.addCustomer(customers);
+
+        // Test printPriceChart()
+        cafe.printPriceChart("Coffee", 2.5, 5);
+
+        // Test printPriceChartWithDiscount()
+        cafe.printPriceChartWithDiscount("Discounted Coffee", 2.5, 5);
+
+        // Test displayMenu() with prices
+        ArrayList<Double> prices = new ArrayList<>(Arrays.asList(2.5, 3.5, 4.5, 5.5));
+        cafe.displayMenu(menu, prices);
+
+        // Test addCustomers() - Adds multiple customers until 'q' is pressed
+        ArrayList<String> multipleCustomers = new ArrayList<>();
+        cafe.addCustomers(multipleCustomers);
+
+        // Note: For the addCustomer() and addCustomers() tests, when running outside of an IDE (like in the terminal/command prompt), the user input should work. However, in many IDEs, System.console().readLine() may not work directly. It's better to use a Scanner object for IDEs.
     }
 }
