5976908a7bf04c9dad398f6b47b50ce290e6944d
John <Johnpua@outlook.com>
2023-10-08 23:46:28 -0700

    Testing

---
+++ Java OOP/ZooKeeper/Zoo/Mammal.java
@@ -0,0 +1,9 @@
+package zoo;
+
+public class Mammal {
+    protected int energy = 100;
+    public int displayEnergy() {
+        System.out.println("Energy level: " + this.energy);
+        return this.energy;
+    }
+}
