615054b0f89ece1f3dfd7a07e155dc546aae0783
John <Johnpua@outlook.com>
2023-10-08 23:49:21 -0700

    Updated and Tested

--- Java OOP/ZooKeeper/Zoo/TestZoo.java
+++ Java OOP/ZooKeeper/Zoo/TestZoo.java
@@ -3,7 +3,7 @@ package zoo;
 public class TestZoo {
 
     public static void main(String[] args) {
-        // Gorilla test
+        // Gorilla tested
         Gorilla g = new Gorilla();
         g.throwSomething();
         g.throwSomething();
@@ -13,7 +13,7 @@ public class TestZoo {
         g.climb();
         g.displayEnergy();
 
-        // Bat test
+        // Bat tested
         Bat b = new Bat();
         b.attackTown();
         b.attackTown();
